c50ed51ddffa3929c5857d522919d454a9b6b4ed
Pontus Lurcock <pont@talvi.net>
2022-01-24 21:33:03 +0100

    Non-YAML configuration, change user-agent, etc.
     - Remove YAML configuration system and use command-line arguments
       instead; remove PyYAML from dependencies.
    
     - Change user-agent for page fetching to a more common one
       (corresponding to Chrome on Windows 10).
    
     - Make output more informative and verbose.
    
     - Add an (as yet ineffective) "cronmode" command-line option.

--- main.py
+++ main.py
@@ -3,34 +3,38 @@
 from typing import Tuple, List
 from bs4 import BeautifulSoup
 from bs4.element import Tag
-import yaml
 import argparse
 import requests
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('config', metavar='YAML_file', type=str)
+    parser.add_argument('vessel', metavar='Vessel-ID', type=str,
+                        help='As it appears in the vesselfinder.com URL')
+    parser.add_argument('bbox', metavar='Bounding-box', type=str,
+                        help='decimal degrees. Format: '
+                             '<min-lon>,<min-lat>,<max-lon>,<max-lat>')
+    parser.add_argument('cronmode', action='store_true',
+                        help='Only write output if vessel is in box.')
     args = parser.parse_args()
-    with open(args.config, 'r') as fh:
-        config = yaml.safe_load(fh)
 
-    content = fetch_page_content(config['vessel_id'])
+    content = fetch_page_content(args.vessel)
     lat, lon = extract_lat_lon(content)
-    lon_min, lat_min, lon_max, lat_max = config['bbox']
-    if lat_min <= lat <= lat_max and \
-            lon_min <= lon <= lon_max:
-        print('Yes!')
-    else:
-        print('No!')
+    lon_min, lat_min, lon_max, lat_max = tuple(map(float, args.bbox.split(',')))
+    vessel_inside = lat_min <= lat <= lat_max and lon_min <= lon <= lon_max
+    print(f'{args.vessel} is at {lat} N {lon} E, '
+          f'{"in" if vessel_inside else "out"}side {args.bbox}')
 
 
 def fetch_page_content(vessel_id: str) -> str:
-    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) ' \
-                 'Gecko/20100101 Firefox/96.0'
-    r = requests.get(f'https://www.vesselfinder.com/vessels/{vessel_id}',
-                     headers={'user-agent': user_agent})
-    return r.text
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+                 'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                 'Chrome/96.0.4664.110 Safari/537.36'
+    response = requests.get(
+        f'https://www.vesselfinder.com/vessels/{vessel_id}',
+        headers={'user-agent': user_agent}
+    )
+    return response.text
 
 
 def extract_lat_lon(page_content: str) -> Tuple[float, float]:
